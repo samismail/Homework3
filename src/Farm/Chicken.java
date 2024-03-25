28a43efe7119fac60bf7d223331ceac83835bc97
samismail <sismail1@ggc.edu>
2024-03-25 11:48:38 -0400

    initial push

---
+++ src/Farm/Chicken.java
@@ -0,0 +1,18 @@
+package Farm;
+
+public class Chicken extends FarmAnimal{
+    private String sound;
+
+    public Chicken(String name, String gender, double weight, int age, String sound) {
+        super(name, gender, weight, age);
+        this.sound = sound;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+}
--- src/Farm/FarmAnimal.java
+++ src/Farm/FarmAnimal.java
@@ -1,4 +1,47 @@
 package Farm;
 
 public class FarmAnimal {
+    private String name;
+    private String gender;
+    private double weight;
+    private int age;
+
+    public FarmAnimal(String name, String gender, double weight, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.weight = weight;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }

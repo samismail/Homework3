e942b773e3605b261c5197faa8a25a0863bb934f
Sam <sismail1@ggc.edu>
2024-03-25 23:58:25 -0400

    initial push

--- src/Exercise2/Airplane.java
+++ src/Exercise2/Airplane.java
@@ -1,4 +1,27 @@
 package Exercise2;
 
 public class Airplane {
+    private String model;
+    private int year;
+
+    public Airplane(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
--- src/Exercise2/Flight.java
+++ src/Exercise2/Flight.java
@@ -1,4 +1,7 @@
 package Exercise2;
 
 public class Flight {
+    public void fly(){
+        return "hi from the sky!!!";
+    }
 }
